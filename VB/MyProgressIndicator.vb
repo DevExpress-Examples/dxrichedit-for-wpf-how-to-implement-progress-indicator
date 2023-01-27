37fe6f4a864793c2dd4bca51e93d33e513bea86b
DevExpressExampleBot <fakeEmail>
2023-01-27 12:08:47 +0400

    Source auto update [skip ci]

--- VB/MyProgressIndicator.vb
+++ VB/MyProgressIndicator.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to implement progress indicator for slow RichEditControl operations
 ' 
 ' This example demonstrates how to utilize the IProgressIndicationService service
@@ -7,65 +7,58 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E3075
-
-
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Text
-Imports DevExpress.XtraEditors
 Imports DevExpress.Services
-Imports System.Windows.Forms
 Imports DevExpress.Xpf.Editors
 Imports System.Windows.Threading
 
 Namespace ProgressIndicator
-	#Region "#myprogressindicator"
-	Friend Class MyProgressIndicatorService
-		Implements IProgressIndicationService
-		Private _Indicator As ProgressBarEdit
-		Public Property Indicator() As ProgressBarEdit
-			Get
-				Return _Indicator
-			End Get
-			Set(ByVal value As ProgressBarEdit)
-				_Indicator = value
-			End Set
-		End Property
 
-		Public Sub New(ByVal provider As IServiceProvider, ByVal indicator As ProgressBarEdit)
-			_Indicator = indicator
-		End Sub
+'#Region "#myprogressindicator"
+    Friend Class MyProgressIndicatorService
+        Implements IProgressIndicationService
 
-		#Region "IProgressIndicationService Members"
+        Private _Indicator As ProgressBarEdit
 
-		Private Sub Begin(ByVal displayName As String, ByVal minProgress As Integer, ByVal maxProgress As Integer, ByVal currentProgress As Integer) Implements IProgressIndicationService.Begin
-			_Indicator.Minimum = minProgress
-			_Indicator.Maximum = maxProgress
-			_Indicator.EditValue = currentProgress
-			_Indicator.Visibility = System.Windows.Visibility.Visible
-			Refresh()
-		End Sub
+        Public Property Indicator As ProgressBarEdit
+            Get
+                Return _Indicator
+            End Get
 
-		Private Sub [End]() Implements IProgressIndicationService.End
-			_Indicator.Visibility = System.Windows.Visibility.Collapsed
-			Refresh()
-		End Sub
+            Set(ByVal value As ProgressBarEdit)
+                _Indicator = value
+            End Set
+        End Property
 
-		Private Sub SetProgress(ByVal currentProgress As Integer) Implements IProgressIndicationService.SetProgress
-			_Indicator.EditValue = currentProgress
-			Refresh()
-		End Sub
-		#End Region
+        Public Sub New(ByVal provider As IServiceProvider, ByVal indicator As ProgressBarEdit)
+            _Indicator = indicator
+        End Sub
 
-		Private Sub Refresh()
-			Dim emptyDelegate As Action = Function() AnonymousMethod1()
-			_Indicator.Dispatcher.Invoke(DispatcherPriority.Render, emptyDelegate)
-		End Sub
-		
-		Private Function AnonymousMethod1() As Boolean
-			Return True
-		End Function
-	End Class
-#End Region ' #myprogressindicator
+'#Region "IProgressIndicationService Members"
+        Private Sub Begin(ByVal displayName As String, ByVal minProgress As Integer, ByVal maxProgress As Integer, ByVal currentProgress As Integer) Implements IProgressIndicationService.Begin
+            _Indicator.Minimum = minProgress
+            _Indicator.Maximum = maxProgress
+            _Indicator.EditValue = currentProgress
+            _Indicator.Visibility = System.Windows.Visibility.Visible
+            Refresh()
+        End Sub
+
+        Private Sub [End]() Implements IProgressIndicationService.End
+            _Indicator.Visibility = System.Windows.Visibility.Collapsed
+            Refresh()
+        End Sub
+
+        Private Sub SetProgress(ByVal currentProgress As Integer) Implements IProgressIndicationService.SetProgress
+            _Indicator.EditValue = currentProgress
+            Refresh()
+        End Sub
+
+'#End Region
+        Private Sub Refresh()
+            Dim emptyDelegate As Action = Sub()
+            End Sub
+            _Indicator.Dispatcher.Invoke(DispatcherPriority.Render, emptyDelegate)
+        End Sub
+    End Class
+'#End Region  ' #myprogressindicator
 End Namespace
